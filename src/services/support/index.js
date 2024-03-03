e7fcb325dfdd83790076c8c85cf26ae633739864
QC <Quangngo1500@gmail.com>
2024-03-03 08:32:48 +0700

    cm

---
+++ src/services/support/index.js
@@ -0,0 +1,17 @@
+import support from '@services/support'
+import axios from 'axios';
+
+const getSupportLink = async () => {
+  let phone = ''
+  const host = support.apiNodeSupport()
+  await axios.get(host + '/contacts').then(response => { phone = 'https://zalo.me/' + response.data[0].phone; })
+  return phone
+}
+
+const apiNodeSupport = () =>
+  'http://localhost:7171'
+
+export default {
+  getSupportLink,
+  apiNodeSupport,
+}
